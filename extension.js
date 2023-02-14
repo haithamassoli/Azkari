5c2d2de100a74d67bef5f2d4db8dac08adeac7a0
haithamassoli <haitham.b.assoli@gmail.com>
2023-02-14 23:24:54 +0300

    fix package.json and delete unused code

--- extension.js
+++ extension.js
@@ -1,18 +1,6 @@
 const vscode = require("vscode");
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("azkari.getAzkari", function () {
-      const azkar = vscode.workspace.getConfiguration("azkari").get("azkar");
-      let azkarIndex = Math.floor(Math.random() * azkar.length);
-      let azkarText = azkar[azkarIndex];
-      vscode.window.showInformationMessage(azkarText);
-    })
-  );
-
   let repeatedEveryMinute = vscode.workspace
     .getConfiguration("azkari")
     .get("repeatedEveryMinute");
