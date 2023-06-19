8befe585e3339fa7afdfe22bf2cca8e28c0f9c93
haithamassoli <haitham.b.assoli@gmail.com>
2023-06-19 07:15:56 +0300

    Added Azkar on Side Bar Item

--- extension.js
+++ extension.js
@@ -25,6 +25,80 @@ function activate() {
       }
     );
   }, convertMinuteToMs);
+  // ===============================================================
+  let currentSide = "left";
+  let currentLanguage = "arabic";
+
+  let AzkarLeftSideBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    0
+  );
+  AzkarLeftSideBarItem.show();
+
+  let AzkarRightSideBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    0
+  );
+  AzkarRightSideBarItem.hide();
+
+  let messages = [
+    "سبحان الله",
+    "الحمد لله",
+    "لا اله الا الله",
+    "الله اكبر",
+    "صلي على النبي ﷺ",
+    "استغفر الله",
+  ];
+  let english_messages = [
+    "Subhan allah",
+    "Alhamdolilah",
+    "La Illaha Illa Allah",
+    "Allahu Akbar",
+    "Astaghfiru Allah",
+  ];
+
+  let index = 0;
+  setInterval(() => {
+    if (currentSide === "left") {
+      if (currentLanguage === "arabic") {
+        AzkarLeftSideBarItem.text = messages[index];
+      } else {
+        AzkarLeftSideBarItem.text = english_messages[index];
+      }
+    } else {
+      if (currentLanguage === "arabic") {
+        AzkarRightSideBarItem.text = messages[index];
+      } else {
+        AzkarRightSideBarItem.text = english_messages[index];
+      }
+    }
+    index = (index + 1) % messages.length;
+  }, autoRemove);
+
+  let toggleSideCommand = vscode.commands.registerCommand(
+    "extension.toggleSide",
+    function () {
+      if (currentSide === "left") {
+        currentSide = "right";
+        AzkarLeftSideBarItem.hide();
+        AzkarRightSideBarItem.show();
+      } else {
+        currentSide = "left";
+        AzkarRightSideBarItem.hide();
+        AzkarLeftSideBarItem.show();
+      }
+    }
+  );
+  let toggleLanguageCommand = vscode.commands.registerCommand(
+    "extension.toggleLanguage",
+    function () {
+      if (currentLanguage === "arabic") {
+        currentLanguage = "english";
+      } else {
+        currentLanguage = "arabic";
+      }
+    }
+  );
 }
 
 function deactivate() {}
